a92ead062ee9e601347be5c9851bfafa68d9b726
David NÃ©grier <d.negrier@thecodingmachine.com>
2017-07-17 10:30:32 +0200

    Methods of extended classes are now properly ignored.

---
+++ tests/Rules/TypeHints/data/StubClass.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace TheCodingMachine\PHPStan\Rules\TypeHints\data;
+
+class ParentStubClass
+{
+    public function bazbaz($no_type_hint) {
+
+    }
+}
+
+
+class StubClass extends ParentStubClass
+{
+    public function baz($no_type_hint) {
+
+    }
+}
